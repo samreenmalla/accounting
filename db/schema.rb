6efe2bbf7f1a13a4a621fefca0d779368ba9f983
Samreen Malla <samreen.malla@gmail.com>
2017-12-27 13:09:33 +0545

    TDD

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171221081943) do
+ActiveRecord::Schema.define(version: 20171221145726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 20171221081943) do
     t.string "date"
     t.string "title"
     t.string "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "records", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
